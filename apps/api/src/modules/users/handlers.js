15e2937b703be76fdd596a5ae29466b1ab10dcec
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-14 15:32:32 -0300

    feat(api): implement sign-up

--- apps/api/src/modules/users/handlers.js
+++ apps/api/src/modules/users/handlers.js
@@ -1,8 +1,23 @@
+import { users } from '#root/db/schema.js';
+import { db } from '#root/lib/drizzle.js';
 import { validate } from '#root/middleware/validations.js';
+import { hash } from '#root/utils/auth.js';
 
 import { schema } from './validations.js';
 
-export const signUp = [validate(schema), async (req, res) => {}];
+export const signUp = [
+	validate(schema),
+	async ({ body: { name, email, password } }, res) => {
+		const hashedPassword = await hash(password);
+
+		const data = await db
+			.insert(users)
+			.values({ name, email, password: hashedPassword })
+			.returning();
+
+		res.send({ data });
+	},
+];
 
 export const signIn = async (req, res) => {};
 
