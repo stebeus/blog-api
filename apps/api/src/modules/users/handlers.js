9fe902f52bd75b61570517f8eec4673d66c39e4e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-14 16:13:15 -0300

    feat(api): check for taken emails

--- apps/api/src/modules/users/handlers.js
+++ apps/api/src/modules/users/handlers.js
@@ -1,3 +1,5 @@
+import { sql } from 'drizzle-orm';
+
 import { users } from '#root/db/schema.js';
 import { db } from '#root/lib/drizzle.js';
 import { validate } from '#root/middleware/validations.js';
@@ -5,9 +7,16 @@ import { hash } from '#root/utils/auth.js';
 
 import { schema } from './validations.js';
 
+const isEmailTaken = (email) =>
+	db.execute(sql`SELECT EXISTS (SELECT 1 FROM users WHERE username = ${email})`);
+
 export const signUp = [
 	validate(schema),
 	async ({ body: { name, email, password } }, res) => {
+		if (isEmailTaken(email)) {
+			return res.status(HTTP_STATUS_BAD_REQUEST).send({ error: 'Email already taken' });
+		}
+
 		const hashedPassword = await hash(password);
 
 		const data = await db
