d73ea5128d5a60287a8469073e4e42752e6f146f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-14 14:56:17 -0300

    feat(api): add `index.js`

---
+++ apps/api/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import { PORT } from './config.js';
+import { logger, pino } from './lib/logger.js';
+import { handleError, handleNotFoundError } from './middleware/errors.js';
+import { api } from './routes.js';
+
+const app = express();
+
+app.use(pino);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/', api);
+
+app.use(handleNotFoundError);
+app.use(handleError);
+
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
