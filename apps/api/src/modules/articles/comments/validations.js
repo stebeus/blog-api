74960b5403a914f7693aebd776322839844dc322
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-15 16:39:47 -0300

    refactor(api): use db column lengths

--- apps/api/src/modules/articles/comments/validations.js
+++ apps/api/src/modules/articles/comments/validations.js
@@ -1,12 +1,15 @@
 import { object, string } from 'zod';
 
+import { comments } from '#root/db/schema.js';
 import { useMax, useMin } from '#root/utils/validations.js';
+
+const { author, content } = comments;
 
 export const schema = object({
 	author: string()
 		.min(...useMin())
-		.max(...useMax(100, 'Your name')),
+		.max(...useMax(author.length, 'Your name')),
 	content: string()
 		.min(...useMin())
-		.max(...useMax(500, 'Comment')),
+		.max(...useMax(content.length, 'Comment')),
 });
--- apps/api/src/modules/articles/validations.js
+++ apps/api/src/modules/articles/validations.js
@@ -1,11 +1,14 @@
 import { object, string, stringbool } from 'zod';
 
+import { articles } from '#root/db/schema.js';
 import { useMax, useMin } from '#root/utils/validations.js';
+
+const { title } = articles;
 
 export const schema = object({
 	title: string()
 		.min(...useMin())
-		.max(...useMax(50, 'Title')),
+		.max(...useMax(title.length, 'Title')),
 	content: string().min(...useMin()),
 	isPublic: stringbool(),
 });
