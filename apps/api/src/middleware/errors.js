f49795aa6b400838afe8b645c53fd394ebeb6a79
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-15 08:58:02 -0300

    feat: add `forbiddenError`

--- apps/api/src/middleware/errors.js
+++ apps/api/src/middleware/errors.js
@@ -2,7 +2,9 @@ import { constants } from 'node:http2';
 
 import { HttpError, isHttpError } from '#root/utils/errors.js';
 
-const { HTTP_STATUS_NOT_FOUND } = constants;
+const { HTTP_STATUS_NOT_FOUND, HTTP_STATUS_FORBIDDEN } = constants;
+
+export const forbiddenError = new HttpError(HTTP_STATUS_FORBIDDEN);
 
 export const handleNotFoundError = (req, res, next) => next(new HttpError(HTTP_STATUS_NOT_FOUND));
 
