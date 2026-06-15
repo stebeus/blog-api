d7208409522e0ade064e3ab76a2caa1528d7fb98
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-15 13:12:47 -0300

    feat(api): enable cors

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 
 import { PORT } from './config.js';
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(pino);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
