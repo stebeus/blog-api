5c66350338e43cac45edabf022cdfb4071191576
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-16 07:47:34 -0300

    fix(api): avoid destructuring to coerce types properly

--- apps/api/src/middleware/validations.js
+++ apps/api/src/middleware/validations.js
@@ -5,15 +5,14 @@ import { flattenError } from 'zod';
 const { HTTP_STATUS_BAD_REQUEST } = constants;
 
 export const validate = (schema) => (req, res, next) => {
-	let { body } = req;
-	const { data, success, error } = schema.safeParse(body);
+	const { data, success, error } = schema.safeParse(req.body);
 
 	if (!success) {
 		const { fieldErrors } = flattenError(error);
 		return res.status(HTTP_STATUS_BAD_REQUEST).send({ errors: fieldErrors });
 	}
 
-	body = data;
+	req.body = data;
 
 	next();
 };
