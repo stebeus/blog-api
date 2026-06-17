96803ac30233481f11a4ec25f901c36e2bc199e6
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-17 09:49:28 -0300

    feat(readers)!: remove error page

--- apps/web/readers/src/routes.jsx
+++ apps/web/readers/src/routes.jsx
@@ -1,16 +1,14 @@
 import { App } from './app.jsx';
 import { Article } from './routes/article.jsx';
-import { ErrorPage } from './routes/error.jsx';
 import { Home } from './routes/home.jsx';
 
 export const routes = [
 	{
 		element: <App />,
-		errorElement: <ErrorPage />,
 		path: '/',
 		children: [
 			{ index: true, element: <Home /> },
-			{ path: 'article', element: <Article /> },
+			{ path: 'article/:id', element: <Article /> },
 		],
 	},
 ];
