e77eba393bc5dd37882da8d27ab5d17331b10cfd
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-17 09:48:23 -0300

    feat(readers): style layout components

--- apps/web/readers/src/components/footer.jsx
+++ apps/web/readers/src/components/footer.jsx
@@ -2,13 +2,13 @@ import { ExternalLink } from './ui/external-link.jsx';
 import { SvgIcon } from './ui/svg-icon.jsx';
 
 export const Footer = () => (
-	<footer>
-		<p>
+	<footer className="flex place-content-center place-items-center border border-border-default border-t p-3">
+		<p className="separator-pipe">
 			© Stebeus 2026.{' '}
 			<ExternalLink to="https://opensource.org/license/mit">MIT License</ExternalLink>
 		</p>
 		<ExternalLink to="https://github.com/stebeus">
-			<SvgIcon title="GitHub" />
+			<SvgIcon title="GitHub" className="transition-spin" />
 		</ExternalLink>
 	</footer>
 );
--- apps/web/readers/src/components/navbar.jsx
+++ apps/web/readers/src/components/navbar.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router';
 
 export const Navbar = () => (
-	<header>
-		<h1 title="Homepage">
+	<header className="sticky top-0 border-border-default border-b bg-base/90 px-4 py-2 backdrop-blur-md">
+		<h1 className="text-xl" title="Homepage">
 			<Link to="/">Blog</Link>
 		</h1>
 	</header>
