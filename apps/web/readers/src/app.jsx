1ce5597b4241c82eac4427bef91a884851cfb80a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-17 09:49:18 -0300

    feat(readers): fetch articles

--- apps/web/readers/src/app.jsx
+++ apps/web/readers/src/app.jsx
@@ -2,13 +2,18 @@ import { Outlet } from 'react-router';
 
 import { Footer } from './components/footer.jsx';
 import { Navbar } from './components/navbar.jsx';
+import { useFetch } from './hooks/fetch.js';
 
-export const App = () => (
-	<>
-		<Navbar />
-		<main>
-			<Outlet />
-		</main>
-		<Footer />
-	</>
-);
+export const App = () => {
+	const [articles, isLoading, error] = useFetch('articles?isPublic=true');
+
+	return (
+		<div className="min-block-screen grid grid-cols-[minmax(0,1fr)] grid-rows-[auto_1fr_auto] gap-4">
+			<Navbar />
+			<main>
+				<Outlet context={[articles, isLoading, error]} />
+			</main>
+			<Footer />
+		</div>
+	);
+};
